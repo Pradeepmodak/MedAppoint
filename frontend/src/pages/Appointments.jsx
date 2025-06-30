bca3e70e0c60122f2cc325b59e53b2aa65edf557
pradeepmodakofficial@gmail.com <pradeepmodakofficial>
2025-07-01 01:12:17 +0530

    Updates All doctors page , doctors id

--- frontend/src/pages/Appointments.jsx
+++ frontend/src/pages/Appointments.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useParams } from 'react-router-dom'
+import { useContext, useState, useEffect } from 'react'
+import { AppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
+const Appointments = () => {
+  const { docId } = useParams();
+  const {doctors,currencySymbol}=useContext(AppContext);
+  const [docInfo,setDocInfo]=useState(null);
+  const [docSlots,setDocSlots]=useState([]);
+  const [slotIndex,setSlotIndex]=useState(0);
+  const [slotTime,setSlotTime]=useState('');
+  const fetchDocInfo=async()=>{
+  const docInf=doctors.find((item)=>item._id===docId);
+  setDocInfo(docInf);
+  // console.log(docInf);
+  }
 
-function Appointments() {
-  return (
-    <div>Appointments</div>
+  const getAvailableSlots=async()=>{
+
+  }
+  
+  useEffect(()=>{
+    fetchDocInfo();
+  },[doctors,docId]);
+  useEffect(()=>{
+    getAvailableSlots();
+  },[docInfo]);
+  return docInfo&&(
+    <div>
+      {/* Doctors Details */}
+      <div className='flex flex-col sm:flex-row gap-4'>
+        <div>
+          <img className='bg-[#5f6FFF] w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
+        </div>
+        <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
+          {/* ------------ Doctors Info: name , degree, experience ------*/}
+          <p className='flex items-center gap-2 text-2xl font-medium text-gray-900'>
+            {docInfo.name} 
+          <img className='w-5' src={assets.verified_icon}/>
+          </p>
+          <div className='flex items-center gap-2 text-sm mt-1 text-gray-600'>
+          <p>{docInfo.degree} - {docInfo.speciality}</p>  
+          <button className='py-0.5 px-2 border text-xs rounded-full cursor-pointer'>{docInfo.experience}</button>
+          </div>
+          {/* ------------ Doctors About ------*/}
+          <div>
+            <p className='flex items-center gap-1 text-sm font-medium text-gray-900 mt-3'>
+              About <img src={assets.info_icon}/></p>
+            <p className='text-sm text-gray-500 max-w-[700px] mt-1'>{docInfo.about}</p>
+          </div>
+          <p className='text-gray-500 font-medium mt-4'>
+            Appointment fee: <span className='text-gray-600'>{currencySymbol}{docInfo.fees}</span>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
