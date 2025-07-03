0dc8e74094d2c854078c5670b903d2bbd2c40bbc
pradeepmodakofficial@gmail.com <pradeepmodakofficial>
2025-07-03 21:38:06 +0530

    All frontend tasks done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -31,9 +31,9 @@ const Navbar=()=>{
    </NavLink>
     </ul>
      
-     <div className='flex items-center gap-4'>
+     <div className='flex items-center gap-4 '>
      {token?
-     <div className='flex items-center gap-2 cursor-pointer group relative'>
+     <div className=' flex items-center gap-2 cursor-pointer group relative '>
         <img className='w-8 rounded-full' src={assets.profile_pic} alt="" />
         <img className='w-2.5 ' src={assets.dropdown_icon} alt="" />
         <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
@@ -48,7 +48,22 @@ const Navbar=()=>{
     <button 
     onClick={() => navigate('/login')}
     className='bg-[#5f6FFF] text-white py-3 px-8 rounded-full font-light hidden md:block '>Create Account</button>
-}</div>
+}
+<img onClick={() => setShowMenu(true) } className='w-6 md:hidden' src={assets.menu_icon} alt="" />
+{/* mobile menu */}
+<div className={`${showMenu?'fixed w-full':'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all duration-300 ease-in-out`}>
+  <div className='flex items-center justify-between px-5 py-6  '>
+      <img className='w-36' src={assets.logo} alt="" />
+      <img  onClick={() => setShowMenu(false) } className='w-7' src={assets.cross_icon} alt="" />
+  </div>
+  <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+    <NavLink  onClick={() => setShowMenu(false) } to={'/'}><p className='px-4 py-2 inline-block'>HOME</p></NavLink>
+    <NavLink  onClick={() => setShowMenu(false) } to={'/doctors'}><p className='px-4 py-2 inline-block'>ALL DOCTORS</p></NavLink>
+    <NavLink  onClick={() => setShowMenu(false) } to={'/about'}><p className='px-4 py-2 inline-block'>ABOUT</p></NavLink>
+    <NavLink  onClick={() => setShowMenu(false) } to={'/contacts'}><p className='px-4 py-2 inline-block'>CONTACT</p></NavLink>
+  </ul>
+</div>
+</div>
     </div>
   )
 }
--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -14,7 +14,7 @@ const RelatedDoctors = ({docId,speciality}) => {
     },[doctors,speciality,docId]);
   return (
         <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
-    <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
+    <h1 className='text-3xl font-medium'>Related Doctors</h1>
     <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
     <div className='w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
     {relDoc.slice(0,5).map((item,index)=>{
