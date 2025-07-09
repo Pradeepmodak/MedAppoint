f753b338491f5bc8ddbd521db594cab4c4144043
pradeepmodakofficial@gmail.com <pradeepmodakofficial>
2025-07-10 01:05:42 +0530

    Admin route fronted - app doctors pagte created successfully

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { addDoctor, loginAdmin } from '../controllers/adminController.js';
+import { addDoctor, loginAdmin , allDoctors} from '../controllers/adminController.js';
 import upload from '../middlewares/multer.js';
 import authAdmin from '../middlewares/authAdmin.js';
 
 const adminRoute = express.Router();
 adminRoute.post('/add-doctor',authAdmin,upload.single('image'), addDoctor);
 adminRoute.post('/login', loginAdmin);
-
+adminRoute.get('/all-doctors',authAdmin,allDoctors);
 export default adminRoute;
